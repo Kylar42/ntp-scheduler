1d73a2c1013ff21bf9327513fc5697a911adb796
Tom Byrne <kylar42@gmail.com>
2013-03-07 19:55:25 -0500

    main frameworks working

---
+++ src/main/java/edu/wvup/cs460/action/ChainStatus.java
@@ -0,0 +1,26 @@
+package edu.wvup.cs460.action;
+
+/**
+ * User: Tom Byrne(tom.byrne@apple.com)
+ * Copyright (C) 2013 Apple Inc.
+ * "Code early, Code often."
+ */
+public enum ChainStatus {
+
+    PASS_CONTINUE(true, true),
+    PASS_DO_NOT_CONTINUE(true, false),
+    FAIL_CONTINUE(false, true),
+    FAIL_DO_NOT_CONTINUE(false, false);
+
+    private boolean _continue;
+    private boolean _success;
+
+    private ChainStatus(boolean shouldContinue, boolean isSuccess){
+        _continue = shouldContinue;
+        _success = isSuccess;
+    }
+
+    public boolean isSuccess(){ return _success;}
+    public boolean shouldContinue(){ return _continue;}//naming convention says it should be 'isContinue' but I like this better semantically.
+
+}
